119e3c97183d87eb3dfae3d0f16c4d4115c53b16
gaikwadChetan93 <gaikwadChetan93@gmail.com>
2017-11-24 15:42:11 +0530

    Testcases added

--- app/src/androidTest/java/com/inteliment/intelimentviewwizard/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/inteliment/intelimentviewwizard/ExampleInstrumentedTest.java
@@ -2,11 +2,21 @@ package com.inteliment.intelimentviewwizard;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.inteliment.intelimentviewwizard.scenario1.DashboardActivity;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -15,12 +25,44 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+public class ExampleInstrumentedTest extends ActivityTestRule<DashboardActivity> {
 
-        assertEquals("com.inteliment.intelimentviewwizard", appContext.getPackageName());
+    @Rule
+    public ActivityTestRule<DashboardActivity> mActivityRule = new ActivityTestRule<>(
+            DashboardActivity.class);
+
+    public ExampleInstrumentedTest() {
+        super(DashboardActivity.class);
+    }
+
+    /**
+     * Test recyclerview item
+     * @throws Exception
+     */
+    @Test
+    public void testRecyclerView() throws Exception {
+
+        onView(withRecyclerView(R.id.list_item)
+                .atPositionOnView(0, R.id.tv_item))
+                .check(matches(withText("Item 1")));
+    }
+
+    /**
+     * Test recyclerview click
+     * @throws Exception
+     */
+    @Test
+    public void testRecyclerViewClick() throws Exception {
+
+        onView(withId(R.id.list_item)).perform(
+                RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        onView(withId(R.id.tvSelectedItem))
+                .check(matches(withText("Item 1")));
+    }
+
+    public static RecyclerViewMatcher withRecyclerView(final int recyclerViewId) {
+
+        return new RecyclerViewMatcher(recyclerViewId);
     }
 }
