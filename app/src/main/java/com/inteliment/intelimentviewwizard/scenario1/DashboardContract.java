eb01f9e924a75ace4534e0a6b768fd0438984140
Chetan Gaikwad <gaikwadChetan93@gmail.com>
2017-12-20 11:49:42 +0530

    Update DashboardContract.java

--- app/src/main/java/com/inteliment/intelimentviewwizard/scenario1/DashboardContract.java
+++ app/src/main/java/com/inteliment/intelimentviewwizard/scenario1/DashboardContract.java
@@ -3,9 +3,8 @@ package com.inteliment.intelimentviewwizard.scenario1;
 import java.util.List;
 
 /**
- * Created by chetan_g on 23/11/17.
+ * Created by chetan on 23/11/17.
  */
-
 interface DashboardContract {
      void showItemList(List<String> trips);
 }
